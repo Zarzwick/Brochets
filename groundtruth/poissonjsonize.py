57546cea7bec0225422b778ba58de443d482013d
Hugo Rens <hugo.rens@laposte.net>
2017-12-22 12:30:55 +0100

    Campaigns 1 and 2 in JSON

---
+++ groundtruth/poissonjsonize.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+#  Convert data from imglab's XML to json
+
+import xml.etree.ElementTree as xml
+import json
+import sys
+
+
+if len(sys.argv) != 2:
+	print('Usage: {} XML_FILE'.format(sys.argv[0]))
+else:
+	xmlFile = sys.argv[1]
+
+# Read XML
+xmlDataset = xml.parse(xmlFile)
+xmlRoot = xmlDataset.getroot()
+xmlImages = xmlRoot[2]
+
+# Create a list from it
+fishes = {}
+
+for xmlImage in xmlImages:
+	fileName = xmlImage.attrib['file']
+	fish = {'id': ''}
+
+	fishBoxes = [node for node in xmlImage if node.tag == 'box']
+
+	for box in fishBoxes:
+		label = box[0].text
+		if label == 'fish':
+			label = 'outer'
+		else:
+			label = 'inner'
+		
+		coords = box.attrib
+		fish[label] = [coords['top'], coords['left'], coords['width'], coords['height']]
+
+	fishes[fileName] = fish
+
+# Dump to JSON
+print(json.dumps(fishes, indent=4, separators=(',', ': ')))
+
