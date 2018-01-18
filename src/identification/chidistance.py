56b8b6b4c8cdfc107a411101e68196503cab5a0c
Hugo Rens <zhugoz.rens@gmail.com>
2018-01-18 13:03:36 +0100

    Fix Chi² distance divby0.

--- src/identification/chidistance.py
+++ src/identification/chidistance.py
@@ -3,7 +3,7 @@
 # This module could be interesting, however it is not weighted.
 #from scipy.stats import chisquare
 
-from numpy import array, shape
+from numpy import array, shape, divide, zeros_like
 
 def weighted_chi_square_distance(h1, h2, regionsWeights) -> float:
     # The goal here is to compute the X² distance between h1 and h2.
@@ -12,7 +12,9 @@ def weighted_chi_square_distance(h1, h2, regionsWeights) -> float:
 
     for region in range(regions):
         w = regionsWeights[region]
-        distance += w * sum((h2[region] - h1[region])**2 / (h2[region] + h1[region]))
+        num = (h2[region] - h1[region])**2
+        den = (h2[region] + h1[region])
+        distance += w * sum(divide(num, den, out=zeros_like(num), where=den!=0))
     
     print(distance)
     return distance
