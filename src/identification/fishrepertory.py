1eefd135bb98191608e87ed65f6f0a28560434c7
Remi Maigne <maigne.remi@gmail.com>
2018-01-18 17:24:27 +0100

    Fishes Log
    Add "suivi" as .csv.
    Add repertory to follow fishes' ID through campaigns.

---
+++ src/identification/fishrepertory.py
@@ -0,0 +1,37 @@
+#!  /usr/bin/env python
+
+import json
+from numpy import array
+from typing import Tuple
+from fish import Fish
+import csv
+
+
+class FishRepertory(object):
+	'''Store the log of fishes.
+	For each fish, we have nCampaigns tuple with the fish ID in each campaign.'''
+
+	def __init__(self, fileName = '../../groundtruth/suivi.csv' ):
+		'''Load the csv of the fishes log.'''
+
+		self.fishesRef = []
+
+		with open( fileName ) as file:
+			file.readline();
+			reader = csv.reader( file )
+
+			for row in reader:
+				countCampaign = len( row ) // 2
+				fish = []
+
+				# Column of ID : 1, 3, 5, 7, ...
+				for c in range(countCampaign):
+					id = row[1 + c * 2]
+
+					if id != '':
+						fish.append( (c+1, int(id) ) )
+					else:
+						fish.append( None )
+
+				self.fishesRef.append( fish )
+
